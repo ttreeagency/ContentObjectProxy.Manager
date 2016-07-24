d2aabbd4d59e94801bac0e3bd3af1e8edd63b2da
Dominique Feyer <dfeyer@ttree.ch>
2016-07-24 17:26:40 +0200

    TASK: Add base exception for the current package

---
+++ Classes/Exception.php
@@ -0,0 +1,30 @@
+<?php
+namespace Ttree\ContentObjectProxy\Manager;
+
+/*
+ * This file is part of the Ttree.ContentObjectProxy.Manager package.
+ *
+ * (c) Contributors of the project and the ttree team - www.ttree.ch
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Error\Message;
+use TYPO3\Flow\Object\ObjectManagerInterface;
+use TYPO3\Flow\Reflection\ReflectionService;
+use TYPO3\Neos\Controller\CreateContentContextTrait;
+use TYPO3\Neos\Controller\Module\AbstractModuleController;
+use TYPO3\Neos\Domain\Service\ContentContextFactory;
+use TYPO3\Neos\Service\UserService;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use TYPO3\TYPO3CR\Domain\Service\ContentProxyableEntityService;
+
+/**
+ * Exception
+ */
+class Exception extends \TYPO3\Flow\Exception
+{
+}
---
+++ Classes/InvalidArgumentException.php
@@ -0,0 +1,21 @@
+<?php
+namespace Ttree\ContentObjectProxy\Manager;
+
+/*
+ * This file is part of the Ttree.ContentObjectProxy.Manager package.
+ *
+ * (c) Contributors of the project and the ttree team - www.ttree.ch
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * InvalidArgumentException
+ */
+class InvalidArgumentException extends Exception
+{
+}
